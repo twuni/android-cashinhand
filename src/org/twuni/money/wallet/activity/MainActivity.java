0dbe9fab15b3238ad507023cc71fa41043ebc035
Leo Accend <github.aeonic@spamgourmet.com>
2011-06-06 17:48:14 +0000

    Running network access in a worker thread and enforcing UI updates to take place in the UI thread. Adding a wallet icon, courtesy of pixel-mixer.com.

--- src/org/twuni/money/wallet/activity/MainActivity.java
+++ src/org/twuni/money/wallet/activity/MainActivity.java
@@ -13,6 +13,7 @@ import org.twuni.money.wallet.TreasuryLocator;
 import org.twuni.money.wallet.util.DebugUtils;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.http.AndroidHttpClient;
@@ -44,16 +45,36 @@ public class MainActivity extends Activity {
 
 	@Override
 	protected void onResume() {
+
 		super.onResume();
-		try {
-			bank.validate();
-		} catch( ManyExceptions exceptions ) {
-			for( Exception exception : exceptions.getExceptions() ) {
-				DebugUtils.handleException( this, exception );
+		final Context context = this;
+
+		new Thread() {
+
+			@Override
+			public void run() {
+				try {
+					bank.validate();
+				} catch( ManyExceptions exceptions ) {
+					for( Exception exception : exceptions.getExceptions() ) {
+						handleException( context, exception );
+					}
+				}
+
+				runOnUiThread( new Runnable() {
+
+					@Override
+					public void run() {
+						TextView balance = (TextView) findViewById( R.id.balance );
+						balance.setText( toCurrencyString( bank.getBalance() ) );
+					}
+
+				} );
+
 			}
-		}
-		TextView balance = (TextView) findViewById( R.id.balance );
-		balance.setText( toCurrencyString( bank.getBalance() ) );
+
+		}.start();
+
 	}
 
 	private String toCurrencyString( int balance ) {
@@ -65,17 +86,28 @@ public class MainActivity extends Activity {
 	}
 
 	public void launchPayment( View view ) {
-		try {
-			int value = getValueFromEditText( R.id.paymentAmount );
-			if( value <= 0 ) {
-				return;
+
+		final Activity context = this;
+
+		new Thread() {
+
+			@Override
+			public void run() {
+				try {
+					int value = getValueFromEditText( R.id.paymentAmount );
+					if( value <= 0 ) {
+						return;
+					}
+					IntentIntegrator.shareText( context, JsonUtils.serialize( bank.withdraw( value ) ) );
+				} catch( InsufficientFunds exception ) {
+					handleException( context, exception );
+				} catch( NetworkError exception ) {
+					handleException( context, exception );
+				}
 			}
-			IntentIntegrator.shareText( this, JsonUtils.serialize( bank.withdraw( value ) ) );
-		} catch( InsufficientFunds exception ) {
-			DebugUtils.handleException( this, exception );
-		} catch( NetworkError exception ) {
-			DebugUtils.handleException( this, exception );
-		}
+
+		}.start();
+
 	}
 
 	@Override
@@ -86,18 +118,29 @@ public class MainActivity extends Activity {
 	}
 
 	private void handleBarcodeScan( IntentResult result ) {
-		String contents = result.getContents();
-		if( contents != null ) {
-			try {
-				bank.deposit( JsonUtils.deserialize( contents, Dollar.class ) );
-			} catch( NetworkError exception ) {
-				DebugUtils.handleException( this, exception );
-			} catch( ClassCastException exception ) {
-				DebugUtils.handleException( this, new IllegalArgumentException( "The barcode you scanned is not a valid dollar.", exception ) );
-			} catch( Exception exception ) {
-				DebugUtils.handleException( this, exception );
+
+		final String contents = result.getContents();
+		final Context context = this;
+
+		new Thread() {
+
+			@Override
+			public void run() {
+				if( contents != null ) {
+					try {
+						bank.deposit( JsonUtils.deserialize( contents, Dollar.class ) );
+					} catch( NetworkError exception ) {
+						handleException( context, exception );
+					} catch( ClassCastException exception ) {
+						handleException( context, new IllegalArgumentException( "The barcode you scanned is not a valid dollar.", exception ) );
+					} catch( Exception exception ) {
+						handleException( context, exception );
+					}
+				}
 			}
-		}
+
+		}.start();
+
 	}
 
 	private int getValueFromEditText( int resourceId ) {
@@ -108,6 +151,15 @@ public class MainActivity extends Activity {
 		} catch( NumberFormatException exception ) {
 			return 0;
 		}
+	}
+	
+	private void handleException( final Context context, final Exception exception ) {
+		runOnUiThread( new Runnable() {
+			@Override
+			public void run() {
+				DebugUtils.handleException( context, exception );
+			}
+		} );
 	}
 
 }
