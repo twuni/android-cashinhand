800fa3ee79f74ffa7a16bff33321df0f7081b71e
Leo Accend <github.aeonic@spamgourmet.com>
2011-08-01 11:49:56 -0700

    Refactoring some of WalletApplication to create activities for result more easily. getBanks() is now more closely connected with the underlying database query so that it can access the treasury URL.

--- src/org/twuni/money/wallet/application/WalletApplication.java
+++ src/org/twuni/money/wallet/application/WalletApplication.java
@@ -18,6 +18,7 @@ import org.twuni.money.wallet.repository.TokenRepository;
 
 import android.app.Activity;
 import android.app.Application;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -112,7 +113,11 @@ public class WalletApplication extends Application {
 
 	public Set<Bank> getBanks() {
 		Set<Bank> banks = new HashSet<Bank>();
-		for( Treasury treasury : getTreasuries() ) {
+		SQLiteDatabase database = repository.getReadableDatabase();
+		Cursor cursor = database.rawQuery( "SELECT DISTINCT treasury FROM token", new String [0] );
+		while( cursor.moveToNext() ) {
+			String treasuryUrl = cursor.getString( 0 );
+			Treasury treasury = new TreasuryClient( client, treasuryUrl );
 			banks.add( getBank( treasury ) );
 		}
 		return banks;
@@ -139,41 +144,39 @@ public class WalletApplication extends Application {
 	public void receive( Activity activity ) {
 		Intent intent = new Intent( Action.RECEIVE.toString() );
 		intent.setType( "text/plain" );
-		activity.startActivityForResult( intent, Request.RECEIVE.hashCode() );
+		startActivityForResult( activity, intent, R.string.receive_money, Request.RECEIVE );
 	}
 
 	public void deposit( Activity activity, String tokenString ) {
-
 		Intent intent = new Intent( activity, DepositActivity.class );
-
 		intent.putExtra( Intent.EXTRA_TEXT, tokenString );
-
-		activity.startActivityForResult( intent, Request.DEPOSIT.hashCode() );
-
+		startActivityForResult( activity, intent, R.string.deposit, Request.DEPOSIT );
 	}
 
 	public void withdraw( Activity activity, int amount, String treasury ) {
-
 		Intent intent = new Intent( activity, WithdrawActivity.class );
-
 		intent.putExtra( Extra.AMOUNT.toString(), amount );
 		intent.putExtra( Extra.TREASURY.toString(), treasury );
-
-		activity.startActivityForResult( intent, Request.WITHDRAW.hashCode() );
-
+		startActivityForResult( activity, intent, Request.WITHDRAW );
 	}
 
 	public void share( Activity activity, String text ) {
-
 		Intent intent = new Intent( Action.SHARE.toString() );
-
 		intent.setType( "text/plain" );
 		intent.putExtra( Intent.EXTRA_TEXT, text );
+		startActivityForResult( activity, intent, R.string.share_via, Request.SHARE );
+	}
 
-		Intent chooser = Intent.createChooser( intent, activity.getString( R.string.share_via ) );
+	private void startActivityForResult( Activity activity, Intent intent, Request request ) {
+		activity.startActivityForResult( intent, request.hashCode() );
+	}
 
-		activity.startActivityForResult( chooser, Request.SHARE.hashCode() );
+	private void startActivityForResult( Activity activity, Intent intent, int chooserLabelId, Request request ) {
+		activity.startActivityForResult( createChooser( activity, intent, chooserLabelId ), request.hashCode() );
+	}
 
+	private Intent createChooser( Context context, Intent intent, int labelId ) {
+		return Intent.createChooser( intent, context.getString( labelId ) );
 	}
 
 }
